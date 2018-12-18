150e11f05e8ae5b5e35efdcd60b6fd85c7fab126
zhen_wang <wzhen0930@163.com>
2018-12-18 19:51:32 +0800

    [WN] 新增json功能 保存功能

--- main.go
+++ main.go
@@ -2,10 +2,33 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"github.com/chromedp/chromedp/runner"
+	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/chromedp/chromedp"
 )
+
+type info struct {
+	Username string `json:username`
+	Password string `json:password`
+}
+
+var userInfo info
+
+func init() {
+	// 初始化用户信息
+	content, err := ioutil.ReadFile("./info.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(content, &userInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	var err error
@@ -16,7 +39,9 @@ func main() {
 
 	// create chrome instance
 	//c, err := chromedp.New(ctxt, chromedp.WithLog(log.Printf))
-	c, err := chromedp.New(ctxt)
+	c, err := chromedp.New(ctxt, chromedp.WithRunnerOptions(
+		runner.Path("C:\\Users\\zhen_wang\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe"),
+	))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,16 +65,18 @@ func main() {
 		log.Fatal(err)
 	}
 
+	ioutil.WriteFile("./resault.txt", []byte(res), os.ModePerm)
 	log.Printf("overview: %s", res)
 }
 
 func text(res *string) chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate("https://trello.com/login"),
-		chromedp.SetValue("#user", "", chromedp.NodeVisible, chromedp.ByQuery),
-		chromedp.SetValue("#password", "", chromedp.NodeVisible, chromedp.ByQuery),
+		chromedp.SetValue("#user", userInfo.Username, chromedp.NodeVisible, chromedp.ByQuery),
+		chromedp.SetValue("#password", userInfo.Password, chromedp.NodeVisible, chromedp.ByQuery),
 		chromedp.Click("#login", chromedp.NodeVisible, chromedp.ByQuery),
 		chromedp.Click(".board-tile-details-name[title='技术分享主题池']>div", chromedp.NodeVisible, chromedp.ByQuery),
-		chromedp.Text(".js-menu-action-list", res, chromedp.NodeVisible, chromedp.ByQuery),
+		chromedp.Click(".show-more", chromedp.NodeVisible, chromedp.ByQuery),
+		chromedp.Text(".phenom-list", res, chromedp.NodeVisible, chromedp.ByQuery),
 	}
 }
